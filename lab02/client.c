2dcb44f44c62d187bb9c2ef859d7ac8baf3589d8
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-10-08 00:58:52 +0300

    Close client socket

--- lab02/client.c
+++ lab02/client.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include "socket.h"
 
 int main(int argc, char *argv[])
@@ -25,9 +26,12 @@ int main(int argc, char *argv[])
 	};
 
 	if (sendto(sockfd, argv[1], strlen(argv[1]), 0, (struct sockaddr *) &server, sizeof server) == -1) {
+		close(sockfd);
 		perror("sendto");
 		return EXIT_FAILURE;
 	}
 
 	tprintf("sent message: %s\n", argv[1]);
+
+	close(sockfd);
 }
